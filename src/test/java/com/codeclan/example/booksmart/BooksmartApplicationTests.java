ed8773b08d4a84abe884a59942f25065c5e113b8
Ian Shepherd <ianshepherd77@gmail.com>
2019-11-04 12:18:24 +0000

    created fin all bookings by customer id text

--- src/test/java/com/codeclan/example/booksmart/BooksmartApplicationTests.java
+++ src/test/java/com/codeclan/example/booksmart/BooksmartApplicationTests.java
@@ -1,5 +1,6 @@
 package com.codeclan.example.booksmart;
 
+import com.codeclan.example.booksmart.models.Booking;
 import com.codeclan.example.booksmart.repositories.BookingRepository.BookingRepository;
 import com.codeclan.example.booksmart.repositories.CustomerRepository.CustomerRepository;
 import com.codeclan.example.booksmart.repositories.MesaRepository.MesaRepository;
@@ -8,6 +9,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 
 @RunWith(SpringRunner.class)
@@ -26,6 +31,14 @@ class BooksmartApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void findBookingsByCustomerId(){
+		List<Booking> found = bookingRepository.findBookingsByCustomerId(1L);
+		assertEquals(3, found.get(0).getNumOfGuests());
+	}
+
+	@Test
 
 //	@Test
 //	public void createMesa() {
