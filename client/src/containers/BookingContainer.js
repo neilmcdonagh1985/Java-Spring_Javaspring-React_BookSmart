0bac9a97006278b00a53af8e328f381a59a59bee
Ian Shepherd <ianshepherd77@gmail.com>
2019-11-05 17:27:58 +0000

    added new booking post

--- client/src/containers/BookingContainer.js
+++ client/src/containers/BookingContainer.js
@@ -11,10 +11,42 @@ class BookingContainer extends Component {
         }
     }
 
+    handleBookingSubmit(formDetail) {
+        fetch("http://localhost:8080/customers", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json", 
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formDetail), 
+        })
+        .then(res => res.json())
+        .then(customer => {
+            fetch("http://localhost:8080/bookings", {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json", 
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    customer: `http://localhost:8080/customers/${customer.id}`,
+                    mesa: `http://localhost:8080/mesas/${formDetail.selectedTableId}`,
+                    date: formDetail.date,
+                    startTime: formDetail.startTime,
+                    endTime: formDetail.endTime,
+                    numOfGuests: formDetail.numOfGuests
+                }), 
+            })
+        })
+
+    }
+
+   
+
     render() {
         return(
             <div className="booking-cont">
-                <NewBookingForm/>
+                <NewBookingForm onBookingSubmit={this.handleBookingSubmit}/>
             </div>
         )
     }
