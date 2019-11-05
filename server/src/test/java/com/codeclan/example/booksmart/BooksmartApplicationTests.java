631490c9d3677b168e1fedd22cb689dba255f806
Ian Shepherd <ianshepherd77@gmail.com>
2019-11-05 11:52:21 +0000

    long id type converted in test

--- server/src/test/java/com/codeclan/example/booksmart/BooksmartApplicationTests.java
+++ server/src/test/java/com/codeclan/example/booksmart/BooksmartApplicationTests.java
@@ -1,6 +1,7 @@
 package com.codeclan.example.booksmart;
 
 import com.codeclan.example.booksmart.models.Booking;
+import com.codeclan.example.booksmart.models.Customer;
 import com.codeclan.example.booksmart.repositories.BookingRepository.BookingRepository;
 import com.codeclan.example.booksmart.repositories.CustomerRepository.CustomerRepository;
 import com.codeclan.example.booksmart.repositories.MesaRepository.MesaRepository;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -35,22 +38,21 @@ class BooksmartApplicationTests {
 	@Test
 	public void findBookingsByCustomerId(){
 		List<Booking> found = bookingRepository.findBookingsByCustomerId(1L);
-		assertEquals(3, found.get(0).getNumOfGuests());
+//		assertEquals(3, found.get(0).getNumOfGuests());
+		assertEquals(new Long(1L), found.get(0).getId());
 	}
 
-//	@Test
+	@Test
+	public void findBookingsByDate(){
+		LocalDate date = LocalDate.of(2017, Month.AUGUST, 23);
+		List<Booking> found = bookingRepository.findBookingsBydate(date);
+		assertEquals(4, found.size());
+	}
 
-//	@Test
-//	public void createMesa() {
-//		Mesa table1 = new Mesa("table1", 4);
-//		mesaRepository.save(table1);
-//	}
-
-//	@Test
-//	public void
-
-
-
-
+	@Test
+	public void findAllCustomersOrderedByBookingSize(){
+	List<Customer> found = customerRepository.findAllCustomersOrderedByBookingSize();
+	assertEquals("Liliana", found.get(0).getName());
+	}
 
 }
