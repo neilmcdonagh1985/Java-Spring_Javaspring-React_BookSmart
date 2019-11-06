86a5368e1e55c673999bbb49c78a91bf10be1c5e
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2019-11-06 12:54:16 +0000

    more bookings put in dataloader

--- server/src/main/java/com/codeclan/example/booksmart/components/DataLoader.java
+++ server/src/main/java/com/codeclan/example/booksmart/components/DataLoader.java
@@ -39,6 +39,9 @@ public class DataLoader implements ApplicationRunner {
         Mesa mesa3 = new Mesa("Table3", 4);
         mesaRepository.save(mesa3);
 
+        Mesa mesa4 = new Mesa("Table4", 6);
+        mesaRepository.save(mesa4);
+
 
 
         Customer customer1 = new Customer("Neil", "07562826673", "neil@hotmail.com" );
@@ -53,12 +56,25 @@ public class DataLoader implements ApplicationRunner {
         Customer customer4 = new Customer("Amanda", "07568997621", "amanda@gmail.com");
         customerRepository.save(customer4);
 
-        LocalTime time1 = LocalTime.of(4, 30, 45);
+        Customer customer5 = new Customer("Jim", "07981726933", "jim@gmail.com");
+        customerRepository.save(customer5);
+
+        Customer customer6 = new Customer("Susan", "07341667245", "Susan@yahoo.co.uk");
+        customerRepository.save(customer6);
+
+        LocalTime time1 = LocalTime.of(4, 30, 00);
         LocalTime time2 = LocalTime.of(10, 45, 00);
+        LocalTime time3 = LocalTime.of(10, 45, 00);
+        LocalTime time4 = LocalTime.of(19, 30, 00);
         LocalTime endTime1 = LocalTime.of(12, 30, 00);
         LocalTime endTime2 = LocalTime.of(13, 15, 00);
+        LocalTime endTime3 = LocalTime.of(21, 15, 00);
+        LocalTime endTime4 = LocalTime.of(22, 00, 00);
         LocalDate date1 = LocalDate.of(2019, Month.NOVEMBER, 05);
         LocalDate date2 = LocalDate.of(2019, Month.NOVEMBER, 07);
+        LocalDate date3 = LocalDate.of(2019, Month.OCTOBER, 21);
+        LocalDate date4 = LocalDate.of(2019, Month.DECEMBER, 23);
+
 
 
         Booking booking1 = new Booking(date1, time1, endTime1, 3, customer1, mesa1);
@@ -73,14 +89,12 @@ public class DataLoader implements ApplicationRunner {
         Booking booking4 = new Booking(date2, time1, endTime2, 5, customer3, mesa1);
         bookingRepository.save(booking4);
 
+        Booking booking5 = new Booking(date4, time4, endTime3, 6, customer4, mesa4);
+        bookingRepository.save(booking5);
 
-
-
-
-
-
-
-
+        Booking booking6 = new Booking(date3, time4, endTime4, 2, customer4, mesa3);
+        bookingRepository.save(booking6);
+        
 
     }
 
