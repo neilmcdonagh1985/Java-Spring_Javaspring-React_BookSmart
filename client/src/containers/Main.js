fbcf3e7a704613d84e6843627a81490a6243ae41
Amanda Williamson <aselliott4@aol.co.uk>
2019-11-06 10:43:27 +0000

    edit booking container created & working

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -4,14 +4,25 @@ import NavBar from '../components/NavBar';
 import BookingContainer from './BookingContainer';
 import CustomersList from '../components/CustomersList';
 import Calendar from '../components/Calendar';
+import EditBookingContainer from './EditBookingContainer';
 // import '../public/style/style.css';
 import '../style/style.css';
 
 class Main extends Component {
 
-    // constructor(props) {
-    //     super(props)
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/bookings')
+            .then(response => response.json())
+            .then(jsonData => this.setState({ data: jsonData['_embedded'].bookings }));
+        console.log("booking data;", this.state.data);
+    }
 
     render() {
         return (
@@ -21,7 +32,7 @@ class Main extends Component {
                     <Switch>
                         <Route path="/bookings" component={Calendar}/>
                         <Route path="/new-booking" component={BookingContainer} />
-                        {/* <Route path="/edit-booking" component={EditBooking}/> */}
+                        <Route path="/edit-booking" render={() => <EditBookingContainer bookings={this.state.data} />} />
                         <Route path="/customers" component={CustomersList} />
                     </Switch>
                 </Fragment>
